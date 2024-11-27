2e288d21d3b92fd1b0c9edc5e039ef6cd392bd7e
OyendunT <oyedunt@gmail.com>
2024-11-27 17:01:51 +0100

    added base file to vite.config

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -10,40 +10,32 @@ import UpdateJob from "../Pages/UpdateJob";
 import Login from "../Components/Login";
 import Signup from "../Components/Signup";
 
-  const routes = createBrowserRouter([
-    {
-      path: "/",
-      element: <App/>,
-      children:[
-        {path: "/", element: <Home/>},
-        {
-          path: "/post-job",
-          element: <CreateJob/>
-        },
-        {
-          path: "/apply-job/:id",
-          element: <ApplyJobForm/>
-        },
-        {
-          path: "/salary",
-          element: <Salary/>
-        },
-        {
-          path: "edit-job/:id",
-          element: <UpdateJob/>,
-          loader: ({params}) => fetch(`https://jobportal-api-mj29.onrender.com/all-jobs/${params.id}`)
-        },
-      ]
-    },
-    {
-      path:"/login",
-      element: <Login/>
-    },
-    {
-      path:"/sign-up",
-      element: <Signup/>
-    }
-  ]);
+const routes = [
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/post-job", element: <CreateJob /> },
+      { path: "/apply-job/:id", element: <ApplyJobForm /> },
+      { path: "/salary", element: <Salary /> },
+      {
+        path: "/edit-job/:id",
+        element: <UpdateJob />,
+        loader: ({ params }) =>
+          fetch(`https://jobportal-api-mj29.onrender.com/all-jobs/${params.id}`),
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/sign-up",
+    element: <Signup />,
+  },
+];
 
   const router = createBrowserRouter(routes, {
     basename: "/JobPortal_Client", // Set the base URL for GitHub Pages
